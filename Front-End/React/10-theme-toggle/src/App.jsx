09ae770b34b8dbb4c2aeec7bda0b54c693183c5f
CWJihad <kmjihadhpl2023@gmail.com>
2026-05-17 15:24:24 +0600

    theme toggle with context api

---
+++ Front-End/React/10-theme-toggle/src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import useTheme, { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
+const App = () => {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const darkMode = () => {
+    setThemeMode("dark");
+  };
+
+  const lightMode = () => {
+    setThemeMode("light");
+  };
+
+  // change theme
+  useEffect(() => {
+    const themeSelect = document.querySelector("html").classList;
+
+    themeSelect.remove("light", "dark");
+
+    themeSelect.add(themeMode);
+
+    console.log('app jsx: ' ,themeMode);
+    
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, darkMode, lightMode }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default App;
